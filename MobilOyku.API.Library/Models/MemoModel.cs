9b5148a24d66fd3c61b1a2e60c35c50bc4cd30ae
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-16 15:47:27 +0300

    models added

---
+++ MobilOyku.API.Library/Models/MemoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilOyku.API.Library.Models
+{
+	public sealed class MemoModel
+	{
+			public int Id { get; init; }
+			public string City { get; set; } = String.Empty;
+			public int UserId { get; set; }
+			private DateTime _CreatedDate;
+
+			public DateTime CreatedDate
+			{
+				get { return _CreatedDate.ToLocalTime(); }
+				set { _CreatedDate = value; }
+			}
+
+
+	}
+}
---
+++ MobilOyku.API.Library/Models/PhotoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilOyku.API.Library.Models
+{
+	public sealed class PhotoModel
+	{
+		public int Id { get; init; }
+
+		public string FilePath { get; set; } = String.Empty;
+
+		private DateTime _CreatedDate;
+
+		public DateTime CreatedDate
+		{
+			get { return _CreatedDate.ToLocalTime(); }
+			set { _CreatedDate = value; }
+		}
+
+	}
+}
