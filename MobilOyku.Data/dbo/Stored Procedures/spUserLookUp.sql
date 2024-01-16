ebbde3575cf1a6f91c57336dfe71147844b9ae3b
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-16 16:39:04 +0300

    user lookup added

---
+++ MobilOyku.Data/dbo/Stored Procedures/spUserLookUp.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookUp]
+	@UserName VARCHAR(50)
+
+AS
+/*
+	EXEC spUserLookUp ''
+*/
+
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT
+	U.Id
+	,U.UserName
+	,U.CreatedDate
+	FROM
+		[dbo].[User] AS U (NOLOCK)
+	WHERE
+	U.UserName = @UserName;
+
+END
