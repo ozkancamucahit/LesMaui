1c6050700a49b9034ad0fe5be1839e33c2c6d045
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-17 12:51:22 +0300

    controller added

---
+++ MobilOYKU.API/Controllers/MemoController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MobilOyku.API.Library.DataAccess;
+using MobilOyku.API.Library.DTOS;
+using MobilOyku.API.Library.Models;
+
+namespace MobilOYKU.API.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public sealed class MemoController : ControllerBase
+	{
+		private readonly IMemoData memoData;
+
+
+		#region CTOR
+		public MemoController(IMemoData memoData)
+        {
+			this.memoData = memoData;
+		}
+		#endregion
+
+
+		[HttpPost]
+		public async Task<IActionResult> SaveMemo(MemoCreateDTO memo)
+		{
+			try
+			{
+				memoData.SaveMemo(memo);
+				return Created();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		
+		[HttpPost]
+		public async Task<IActionResult> RemoveMemo(MemoDeleteDTO memo)
+		{
+			try
+			{
+				memoData.RemoveMemo(memo);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		
+		[HttpGet]
+		[Route("{UserName}")]
+		public async Task<ActionResult<IEnumerable<MemoReadDTO>>> UserMemos(string UserName)
+		{
+			try
+			{
+				var result =  memoData.GetUserMemos(UserName);
+				return Ok(result);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+
+    }
+}
---
+++ MobilOYKU.API/Controllers/PhotoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MobilOyku.API.Library.DataAccess;
+using MobilOyku.API.Library.DTOS;
+
+namespace MobilOYKU.API.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public sealed class PhotoController : ControllerBase
+	{
+		private readonly IPhotoData photoData;
+
+		#region CTOR
+		public PhotoController(IPhotoData photoData)
+        {
+			this.photoData = photoData;
+		}
+		#endregion
+
+
+		[HttpPost]
+		public async Task<IActionResult> SavePhoto(PhotoCreateDTO photo)
+		{
+			try
+			{
+				var result = photoData.SavePhoto(photo);
+
+				if (result)
+					return Ok();
+				else 
+					return BadRequest("COULD NOT SAVE PHOTO");
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		
+		[HttpPost]
+		public async Task<IActionResult> RemovePhoto(PhotoDeleteDTO photo)
+		{
+			try
+			{
+				var result = photoData.RemovePhoto(photo);
+
+				if (result)
+					return Ok();
+				else 
+					return BadRequest("COULD NOT SAVE PHOTO");
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+	}
+}
