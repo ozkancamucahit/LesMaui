2cd78651cd55742017d78aeabf4ef9fd7acd0914
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-17 12:50:51 +0300

    api helpers added

---
+++ UI.Library/API/APIHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.API
+{
+
+	public sealed class APIHelper : IAPIHelper
+	{
+		#region FIELDS
+		private HttpClient apiClient;
+		#endregion
+
+
+		public HttpClient ApiClient => apiClient;
+
+
+        #region CTOR
+        public APIHelper()
+        {
+			string api = "http://localhost:5124"; //ConfigurationManager.AppSettings["api"] ?? "";
+
+			apiClient = new HttpClient();
+			apiClient.BaseAddress = new Uri(api);
+			apiClient.DefaultRequestHeaders.Accept.Clear();
+			apiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+		}
+        #endregion
+
+    }
+}
