407eff1ead66c166d965b788de9146302e8f4a31
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-20 01:25:37 +0300

    user end points added

---
+++ UI.Library/API/IUserEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using MobilOyku.API.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.API
+{
+	public interface IUserEndPoint
+	{
+
+		Task <UserModel> GetUser(string UserName);
+
+		Task <bool> SaveUser(string UserName);
+
+	}
+}
---
+++ UI.Library/API/UserEndPoint.cs
@@ -0,0 +1,76 @@
+﻿using MobilOyku.API.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Library.API
+{
+	public class UserEndPoint : IUserEndPoint
+	{
+		private readonly IAPIHelper apiHelper;
+
+		#region CTOR
+		public UserEndPoint(IAPIHelper apiHelper)
+        {
+			this.apiHelper = apiHelper;
+		}
+        #endregion
+
+
+        public async Task<UserModel> GetUser(string UserName)
+		{
+			try
+			{
+				using (HttpResponseMessage response = await apiHelper.ApiClient.GetAsync($"/api/Memo/UserMemos/{UserName}"))
+				{
+					if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+					{
+						return new UserModel();
+					}
+
+					else if (response.IsSuccessStatusCode)
+					{
+						var result = await response.Content.ReadAsAsync<UserModel>();
+						return result;
+					}
+					else
+					{
+						throw new InvalidOperationException(response.ReasonPhrase);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return new UserModel();
+			}
+		}
+
+		public async Task<bool> SaveUser(string UserName)
+		{
+			var request = new { UserName };
+
+			try
+			{
+				using (HttpResponseMessage response = await apiHelper.ApiClient.PostAsJsonAsync("/api/User/SaveUser", request))
+				{
+
+					if (response.IsSuccessStatusCode)
+					{
+						//TODO: Log Call
+						return true;
+					}
+					else
+					{
+						throw new InvalidOperationException(response.ReasonPhrase);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+	}
+}
